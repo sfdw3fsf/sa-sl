2656f87a6c8b50afe73c7fe899ebf1b79af00b49
VawnHuy <huy820op@gmail.com>
2025-02-02 18:48:09 +0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Switzland Service greet you !</h1>
+    </>
+  );
+}
+
+export default App;
