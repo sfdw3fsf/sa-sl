4b6f92943cf030090cc2503060e73a13d175b453
VawnHuy <huy820op@gmail.com>
2025-02-10 15:05:02 +0700

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function Hi(num: any, string) {
-  const h = num + string;
+    const h = num + string
 }
 
 function App() {
-  const [color, setC] = useState();
-  const [t, setT] = useState(false);
+    const [color, setC] = useState()
+    const [t, setT] = useState(false)
+    const [string, setTring] = useState('hi')
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Switzland Service greet you !</h1>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+            </div>
+            <h1>Switzland Service greet you !</h1>
+            <p className="hi there">""</p>
+        </>
+    )
 }
 
-export default App;
+export default App
